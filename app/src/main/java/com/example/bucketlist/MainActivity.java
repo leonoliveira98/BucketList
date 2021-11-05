93686cda0ef4cb23ac7c023a3ea4c8e117f2e5eb
Leonardo Oliveira <lolive4@sibsnet.local>
2021-11-05 17:52:05 +0000

    XML Changes and getting views

--- app/src/main/java/com/example/bucketlist/MainActivity.java
+++ app/src/main/java/com/example/bucketlist/MainActivity.java
@@ -1,21 +1,53 @@
 package com.example.bucketlist;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    CardView toGoCardView;
+    CardView toDoCardView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViews();
+
+        cardClick();
+    }
+
+    private void cardClick() {
+        toGoCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent toGoActivity = new Intent(MainActivity.this, toGoActivity.class);
+                //startActivity(toGoActivity);
+
+            }
+        });
+
+        toDoCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent toDoActivity = new Intent(MainActivity.this, toDoActivity.class);
+                startActivity(toDoActivity);
+
+            }
+        });
+
+
     }
 
     private void findViews(){
 
+        toGoCardView = findViewById(R.id.card_view_toGo);
+        toDoCardView = findViewById(R.id.card_view_toDo);
 
     }
 }
---
+++ app/src/main/java/com/example/bucketlist/toDoActivity.java
@@ -0,0 +1,14 @@
+package com.example.bucketlist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class toDoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_to_do);
+    }
+}
